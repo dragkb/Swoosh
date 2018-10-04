28a6ba3040809e9df5fbea15dd93714e82dcfa5d
dragkb <alex_kotsar@outlook.com>
2018-10-04 12:07:05 -0700

    Implemented click to WelcomeActivity

--- app/src/main/java/com/alex/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/alex/swoosh/LeagueActivity.kt
@@ -3,10 +3,11 @@ package com.alex.swoosh
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class LaegueActivity : AppCompatActivity() {
+class LeagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_laegue)
     }
+
 }
