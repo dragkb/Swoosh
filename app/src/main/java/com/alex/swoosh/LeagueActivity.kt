ed6ad8883fe5beb333594ebee7271bceffebd09c
dragkb <alex_kotsar@outlook.com>
2018-10-04 15:39:30 -0700

    SkillActivity and click on nextBtn

--- app/src/main/java/com/alex/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/alex/swoosh/LeagueActivity.kt
@@ -1,12 +1,19 @@
 package com.alex.swoosh
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_laegue)
+        setContentView(R.layout.activity_league)
+    }
+
+    fun leagueNextBtnClicked(view: View){
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
     }
 
 }
