19db813489b4eed06eb8c2eb49b53b81d1ef43bf
dragkb <alex_kotsar@outlook.com>
2018-10-03 17:28:43 -0700

    League Activity created

---
+++ app/src/main/java/com/alex/swoosh/LaegueActivity.kt
@@ -0,0 +1,12 @@
+package com.alex.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LaegueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_laegue)
+    }
+}
